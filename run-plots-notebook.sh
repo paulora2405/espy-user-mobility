d6937b07b1164f798049ddb02004444974ebf591
Paulo Albuquerque <paulora2405@gmail.com>
2024-11-30 16:32:08 +0000

    Refactor plots script

--- run-plots-notebook.sh
+++ run-plots-notebook.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-jupyter nbconvert --to python --execute espy_user_mobility/espy-notebooks/results_plots.ipynb
+log_dirs=$(find logs -type f -name "User.csv" -exec dirname {} \; | sed 's|^logs||' | sort | uniq)
+script=$(jupyter nbconvert --to python --stdout espy_user_mobility/espy-notebooks/results_plots.ipynb)
+
+while IFS= read -r line; do
+    export DIR_SUFFIX="$line"
+    echo "Processing $line"
+    echo "$script" | python3 && echo "$line" >> done-dirs.txt
+    echo
+done <<< "$log_dirs"
