6ec9e41d1057ac5d58cdc49f5ce82a97b6f4dff3
Paulo Albuquerque <paulora2405@gmail.com>
2024-12-01 17:15:02 +0000

    Refactor script

--- run-plots-notebook.sh
+++ run-plots-notebook.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+export PROJ_DIR=$(dirname "$(readlink -f "$0")")
+source .venv/bin/activate
 log_dirs=$(find logs -type f -name "User.csv" -exec dirname {} \; | sed 's|^logs||' | sort | uniq)
-script=$(jupyter nbconvert --to python --stdout espy_user_mobility/espy-notebooks/results_plots.ipynb)
+plots_script=$(jupyter nbconvert --to python --stdout espy_user_mobility/espy-notebooks/results_plots.ipynb)
+cdfs_script=$(jupyter nbconvert --to python --stdout espy_user_mobility/espy-notebooks/cdf_plots.ipynb)
 
 while IFS= read -r line; do
     export DIR_SUFFIX="$line"
     echo "Processing $line"
-    echo "$script" | python3 && echo "$line" >> done-dirs.txt
+    echo "$plots_script" | python3
     echo
 done <<< "$log_dirs"
+
+echo "Processing CDFs"
+echo "$cdfs_script" | python3
